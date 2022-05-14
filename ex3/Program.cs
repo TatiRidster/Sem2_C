3a4b70c5345d58f3b0553ce0cc83b1fe5b05bfc5
Tatiana <tatnovid@gmail.com>
2022-05-14 21:05:46 +0300

    задача 15 про дни недели

---
+++ ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите цифру обозначающую день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+bool n1 = (number < 1) | (number > 7);
+bool n2 = (n1 == false) & (number < 6);
+bool n3 = (n1 == false) & (number > 5);
+
+
+if (n2 == true)
+{
+    Console.WriteLine("это рабочий день");
+}
+else if (n3 == true)
+{
+    Console.WriteLine("это выходной");
+}
+
+else if (n1 == true)
+{
+    Console.WriteLine("число введено неверно");
+}
+else
+{
+    Console.WriteLine("логический абсурд");
+}
